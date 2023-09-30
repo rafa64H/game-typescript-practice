cbe4a73b3f251634de9b1eeed0987f32ae042344
Rafael Pacheco <rafaelpache64@outlook.com>
2023-09-30 16:32:39 -0400

    I added movement to the players

--- basic-game/src/classes.ts
+++ basic-game/src/classes.ts
@@ -23,18 +23,29 @@ export class CanvasView {
 }
 
 export class Player {
+  public playerSpeedX: number;
+  public playerSpeedY: number;
+  public movingUp: boolean;
+  public movingRight: boolean;
+  public movingDown: boolean;
+  public movingLeft: boolean;
   constructor(
+    public parentCanvas: CanvasView,
     public playerPosition: { x: number; y: number },
     public playerWidth: number,
     public playerHeight: number,
-    public playerSpeedX: number,
-    public playerSpeedY: number
+    public player: number
   ) {
     this.playerPosition = playerPosition;
     this.playerWidth = playerWidth;
     this.playerHeight = playerHeight;
-    this.playerSpeedX = playerSpeedX;
-    this.playerSpeedY = playerSpeedY;
+    this.playerSpeedX = 20;
+    this.playerSpeedY = 0;
+    this.movingUp = false;
+    this.movingRight = false;
+    this.movingDown = false;
+    this.movingLeft = false;
+    this.player = player;
 
     // Event Listeners
     document.addEventListener('keydown', this.handleKeyDown);
@@ -52,24 +63,71 @@ export class Player {
     );
   }
 
-  jumpPlayer(): void {
-    this.playerSpeedY = -10;
+  movePlayer(): void {
+    if (this.movingUp) {
+      this.playerSpeedY = -10;
+      this.playerPosition.y += this.playerSpeedY;
+    }
+    if (this.movingRight) {
+      this.playerPosition.x += this.playerSpeedX;
+    }
+    if (this.movingDown) {
+      this.playerSpeedY += 2;
+    }
+    if (this.movingLeft) {
+      this.playerPosition.x += -this.playerSpeedX;
+    }
 
-    this.playerPosition.y += this.playerSpeedY;
+    // if (this.movingUp && this.movingRight) {
+    //   this.playerSpeedY = -10;
+    //   this.playerPosition.y += this.playerSpeedY;
+    //   this.playerPosition.x += this.playerSpeedX;
+    // }
   }
 
-  handleKeyUp = (e: KeyboardEvent): void => {
-    // if (e.code === 'ArrowLeft' || e.key === 'ArrowLeft') this.moveLeft = false;
-    // if (e.code === 'ArrowRight' || e.key === 'ArrowRight')
-    //   this.moveRight = false;
+  handleKeyDown = (e: KeyboardEvent): void => {
+    e.preventDefault();
 
-    if (e.code === 'KeyW' || e.key === 'w') this.jumpPlayer();
+    console.log(e.key);
+    //Player one keys:
+    if ((e.key === 'W' || e.key === 'w') && this.player === 1) {
+      this.movingUp = true;
+    }
+    if ((e.key === 'D' || e.key === 'd') && this.player === 1) {
+      this.movingRight = true;
+    }
+    if ((e.key === 'S' || e.key === 's') && this.player === 1) {
+      this.movingDown = true;
+    }
+    if ((e.key === 'A' || e.key === 'a') && this.player === 1) {
+      this.movingLeft = true;
+    }
+
+    //Player two keys:
+    if (e.code === 'ArrowUp' && this.player === 2) {
+    }
   };
 
-  handleKeyDown = (e: KeyboardEvent): void => {
-    // if (e.code === 'ArrowLeft' || e.key === 'ArrowLeft') this.moveLeft = true;
-    // if (e.code === 'ArrowRight' || e.key === 'ArrowRight')
-    //   this.moveRight = true;
+  handleKeyUp = (e: KeyboardEvent): void => {
+    e.preventDefault();
+
+    //Player one keys:
+    if ((e.key === 'W' || e.key === 'w') && this.player === 1) {
+      this.movingUp = false;
+    }
+    if ((e.key === 'D' || e.key === 'd') && this.player === 1) {
+      this.movingRight = false;
+    }
+    if ((e.key === 'S' || e.key === 's') && this.player === 1) {
+      this.movingDown = false;
+    }
+    if ((e.key === 'A' || e.key === 'a') && this.player === 1) {
+      this.movingLeft = false;
+    }
+
+    //Player two keys:
+    if (e.code === 'ArrowUp' && this.player === 2) {
+    }
   };
 }
 
@@ -84,7 +142,30 @@ export class Gravity {
         player.playerPosition.y += player.playerSpeedY;
       } else {
         player.playerSpeedY = 0;
-        player.playerPosition.y = canvas.height - player.playerHeight;
+      }
+    });
+  }
+}
+
+export class Collision {
+  collisionOnBorders(canvas: CanvasView, playersArr: Player[]): void {
+    playersArr.forEach((player) => {
+      const borderTop = 0;
+      const borderRight = canvas.width - player.playerWidth;
+      const borderBottom = canvas.height - player.playerHeight;
+      const borderLeft = 0;
+
+      if (player.playerPosition.x <= borderLeft) {
+        player.playerPosition.x = borderLeft;
+      }
+      if (player.playerPosition.x >= borderRight) {
+        player.playerPosition.x = borderRight;
+      }
+      if (player.playerPosition.y <= borderTop) {
+        player.playerPosition.y = borderTop;
+      }
+      if (player.playerPosition.y >= borderBottom) {
+        player.playerPosition.y = borderBottom;
       }
     });
   }
--- basic-game/src/main.ts
+++ basic-game/src/main.ts
@@ -1,13 +1,21 @@
-import { CanvasView, Gravity, Player } from './classes';
+import { CanvasView, Collision, Gravity, Player } from './classes';
 
 const canvas = new CanvasView('game-view');
 
 canvas.create();
 
-const playerOne = new Player({ x: 0, y: 0 }, 25, 100, 0, 0);
-const playerTwo = new Player({ x: canvas.width - 40, y: 0 }, 25, 100, 0, 0);
+const playerOne = new Player(canvas, { x: 0, y: 0 }, 25, 100, 1);
+const playerTwo = new Player(
+  canvas,
+  { x: canvas.width - 40, y: 0 },
+  25,
+  100,
+  2
+);
 
 const gravityInstance = new Gravity();
+
+const collisionInstance = new Collision();
 
 function gameLoop() {
   canvas.clear();
@@ -15,8 +23,12 @@ function gameLoop() {
 
   playerOne.drawPlayer(canvas);
   playerTwo.drawPlayer(canvas);
+  playerOne.movePlayer();
+  playerTwo.movePlayer();
 
   gravityInstance.gravityOnDrawings(canvas, [playerOne, playerTwo]);
+
+  collisionInstance.collisionOnBorders(canvas, [playerOne, playerTwo]);
 
   requestAnimationFrame(() => {
     gameLoop();
